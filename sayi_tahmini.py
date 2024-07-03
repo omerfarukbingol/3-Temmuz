ccd28e3bc60554f92e4081041600c793d2d91800
omer <omerbngl9090@gmail.com>
2024-07-03 10:06:22 +0300

    3 Temmuz

---
+++ sayi_tahmini.py
@@ -0,0 +1,18 @@
+import random
+
+# 0-100 arasında random bir sayı ürettik.
+randomNumber=random.randint(1,100)
+# print(randomNumber)
+
+t=0
+
+tahminAdedi=int(input('Kaç adet hakkınız olsun? '))
+for i in range(1,tahminAdedi+1):
+    sayi=int(input('Bir adet sayı giriniz: '))
+    if(sayi==randomNumber):
+        print("Tebrikler sayı bildiniz")
+        t+=1
+        break
+
+if(t!=1):
+    print("Maalesef sayıyı bilemediniz")
